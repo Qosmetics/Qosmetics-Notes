e6931dedf3793b2c89cf49b524b12d6e45f2d5c8
RedBrumbler <redbrumbler.official+github@gmail.com>
2022-01-24 17:15:03 +0100

    Add lauries asset include stuff

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,9 @@
 #include "HMUI/CurvedTextMeshPro.hpp"
 #include "HMUI/ImageView.hpp"
 
+#include "qosmetics-core/shared/FlowCoordinatorRegister.hpp"
 #include "qosmetics-core/shared/RedecorationRegister.hpp"
+#include "questui/shared/BeatSaberUI.hpp"
 
 #include "GlobalNamespace/BeatmapObjectsInstaller.hpp"
 #include "GlobalNamespace/GameNoteController.hpp"
@@ -14,12 +16,25 @@
 #include "UnityEngine/Transform.hpp"
 #include "UnityEngine/Vector3.hpp"
 
-#include "CustomTypes/Cyoob.hpp"
+#include "CustomTypes/CyoobParent.hpp"
+#include "UI/CyoobFlowCoordinator.hpp"
+#include "assets.hpp"
+
+#include <vector>
 
 REDECORATION_REGISTRATION(normalBasicNotePrefab, 10, true, GlobalNamespace::GameNoteController*, GlobalNamespace::BeatmapObjectsInstaller*)
 {
-    normalBasicNotePrefab->get_gameObject()->AddComponent<Qosmetics::Notes::Cyoob*>();
+    normalBasicNotePrefab->get_gameObject()->AddComponent<Qosmetics::Notes::CyoobParent*>();
     return normalBasicNotePrefab;
+}
+
+QOSMETICS_FLOWCOORDINATOR_REGISTER(Cyoobs, Qosmetics::Notes::CyoobFlowCoordinator*)
+{
+    auto inactive_data = NoteIcon_png::getData();
+    auto inactive = QuestUI::BeatSaberUI::VectorToSprite(std::vector<uint8_t>(inactive_data, inactive_data + NoteIcon_png::getLength()));
+    auto active_data = NoteIconSelected_png::getData();
+    auto active = QuestUI::BeatSaberUI::VectorToSprite(std::vector<uint8_t>(active_data, active_data + NoteIconSelected_png::getLength()));
+    return std::make_pair(inactive, active);
 }
 
 ModInfo modInfo = {ID, VERSION};
